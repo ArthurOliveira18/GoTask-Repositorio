6bc81e6fbfeaa649f424119eaa468bee467474e4
Arthur Evangelista Oliveira <arthur.e.oliveira@aluno.senai.br>
2024-09-26 17:15:04 -0300

    Quase finalizando a tela de login, faltando deixar responsiva

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FormLogin from './components/FormLogin'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,32 +1,27 @@
 import Style from '../styles/FormLogin.module.css';
 
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 
 const FormLogin = () => {
   return (
     <div className = {Style.formMain}>
-      <Form className={Style.formLogin}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Digite o seu email" />
+      <form >
 
-        </Form.Group>
 
-        <div className={Style.groupButton}>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Senha</Form.Label>
-            <Form.Control type="password" placeholder="Digite sua senha" />
-          </Form.Group>
-          
-        </div> 
-
-        <div className={Style.buttonLayout}>
-          <Button variant="primary" type="submit">
-              Enviar-me
-            </Button>
+      <label htmlFor="email">Email</label>
+        <div className={Style.divEmail}>
+          <input type="email" name="email" id="email" required/>
         </div>
-      </Form>
+
+        <label htmlFor="password">Senha</label>
+        <div className={Style.divPassword}>
+          <input type="password" name="password" id="password" />
+          <button type='submit' id='oii'>Entrar</button>
+        </div>
+        
+        
+
+      </form>
+      
     </div>
   )
 }
