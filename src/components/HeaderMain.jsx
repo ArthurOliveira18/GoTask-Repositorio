295ee2f680d1b2fb07bf5c5a6b2ce72bbf619962
Arthur Evangelista <arthur.e.oliveira@aluno.senai.br>
2024-09-28 07:10:49 -0300

    Tela de login finalizada

--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -1,11 +1,13 @@
 import Style from '../styles/HeaderMain.module.css'
 import HeaderP from '../assets/TATURANA.png'
+import GoTask from '../assets/logoGT.png'
 
 const HeaderMain = () => {
   return (
     <div className={Style.headerMain}>
-        <img src = {HeaderP} alt="" className={Style.img}/>
-        
+        <img src = {HeaderP} alt="" className={Style.imgPurple}/>
+        <img src={GoTask} alt=""  className={Style.imgGoTask}/>
+
     </div>
   )
 }
