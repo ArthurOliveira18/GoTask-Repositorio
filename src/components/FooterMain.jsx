b7498eb666de434d107a11553f3a2c6b23ac7f8c
Arthur Evangelista <arthur.e.oliveira@aluno.senai.br>
2024-10-01 10:49:12 -0300

    Tentando adicionar icones na tela que por algum caralho neste inferno nao quer ir.

--- src/components/FooterMain.jsx
+++ src/components/FooterMain.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Style from '../styles/FooterMain.module.css'
+import imgFooterMain from '../assets/Mamamia.png'
 
 const FooterMain = () => {
   return (
-    <div>FooterMain</div>
+    <div>
+        <div className={Style.divFooterMain}>
+            <img src={imgFooterMain} alt="" className={Style.imgMainFooter} />
+
+            <span class="material-symbols-outlined">
+                calendar_today
+            </span>
+        </div>
+    </div>
   )
 }
 
