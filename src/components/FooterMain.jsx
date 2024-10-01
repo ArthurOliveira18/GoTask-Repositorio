c7538dda275be42271e2a1cb7e0ad9f5847cce25
Arthur Evangelista <arthur.e.oliveira@aluno.senai.br>
2024-10-01 08:57:57 -0300

    Mexendo nesta branch

---
+++ src/components/FooterMain.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Style from '../styles/FooterMain.module.css'
+
+const FooterMain = () => {
+  return (
+    <div>FooterMain</div>
+  )
+}
+
+export default FooterMain
