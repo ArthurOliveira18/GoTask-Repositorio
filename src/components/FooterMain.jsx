d2a1482777e21c8ac59394c3a45e7ce923a1ae3d
Arthur Evangelista <arthur.e.oliveira@aluno.senai.br>
2024-10-01 20:19:35 -0300

    Finalizando o footer e faltando apenas uma animação quando troca de pagina

--- src/components/FooterMain.jsx
+++ src/components/FooterMain.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import Style from '../styles/FooterMain.module.css'
-import imgFooterMain from '../assets/Mamamia.png'
+import React from 'react';
+import Style from '../styles/FooterMain.module.css';
+import imgFooterMain from '../assets/Mamamia.png';
 
 const FooterMain = () => {
   return (
     <div>
-        <div className={Style.divFooterMain}>
-            <span class="material-symbols-outlined">
-                calendar_today
-            </span>
-        </div>
+      <div className={Style.divFooterMain}>
+        <span className="material-symbols-outlined">
+          calendar_today
+        </span>
+
+        <span className="material-symbols-outlined">
+          shopping_cart
+        </span>
+
+        <span className={"material-symbols-outlined"}>
+          home
+        </span>
+
+        <span className="material-symbols-outlined">
+          settings
+        </span>
+
+        <span className="material-symbols-outlined">
+          family_restroom
+        </span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default FooterMain
+export default FooterMain;
