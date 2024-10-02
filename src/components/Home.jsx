1211a36b1658d41d3d531500478c233a1a5a149b
Arthur Evangelista <arthur.e.oliveira@aluno.senai.br>
2024-10-01 21:10:30 -0300

    Iniciando a tela Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,38 @@
-import Style from '../styles/Home.module.css'
-import FooterMain from './FooterMain'
-import HeaderMain from './HeaderMain'
-
+import React from 'react';
+import Style from '../styles/Home.module.css';
+import FooterMain from './FooterMain';
+import HeaderMain from './HeaderMain';
 
 const Home = () => {
   return (
-    <div>
-        <HeaderMain/>
+    <div className={Style.pageContainer}> {/* Classe para garantir layout flex */}
+      <HeaderMain/>
 
-        <div className={Style.mainHome}>
-            a
+      
+      <div className={Style.homeMain}>
+
+        {/* Essas divs que estão com nomes de cor são apenas para eu conseguir colocar uma cor de fundo nelas como está no protótipo */}
+        <div className={Style.homeBlue}>
+          
+          <div className={Style.homePurple}>
+
+            {/* Essa div aqui são com as informações dos filhos, por isso o nome de "homeChildren" */}
+            <div className={Style.homeChildren}>
+              <div className={Style.nameChildren}>
+                <h2>Cristhiano</h2>
+                
+                <p>total de pontos: 50</p>
+                <hr />
+              </div>
+            </div>
+          </div>
         </div>
 
-        <FooterMain/>
+      </div>
+
+      <FooterMain/>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
