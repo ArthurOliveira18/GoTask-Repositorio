b0639296e65ed6d2f17e415a32737dfc362bdef6
Arthur Evangelista Oliveira <arthur.e.oliveira@aluno.senai.br>
2024-10-03 17:20:19 -0300

    Iniciando tela de CardChildren

--- src/components/CardChildren.jsx
+++ src/components/CardChildren.jsx
@@ -4,8 +4,11 @@ import style from '../styles/CardChildren.module.css';
 const CardChildren = () => {
   return (
     <div className={style.homeMain}>
-      <h1>Conteúdo principal</h1>
-      <h1>Conteudo secundário</h1>
+      <div className={style.homePurple}>
+        <div className={style.homeChildren}>
+          <p>a</p>
+        </div>
+      </div>
     </div>
   );
 };
