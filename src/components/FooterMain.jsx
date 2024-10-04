633d01785c9fc68dad0d3e86e5dbc0fa368cb78a
MIGUEL KNUPP AYRES BARBOZA <mbarboza@labcetec.net>
2024-10-04 16:57:43 -0300

    Feito com successo a navegação do footter com navigations

--- src/components/FooterMain.jsx
+++ src/components/FooterMain.jsx
@@ -1,30 +1,55 @@
 import React from 'react';
 import Style from '../styles/FooterMain.module.css';
+import { useLocation } from 'react-router-dom';
+import { CalendarToday, ShoppingCart, Home, Settings, FamilyRestroom } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+
 
 const FooterMain = () => {
-  return (
-    <div className={Style.divFooterMain}>
-      <span className="material-symbols-outlined">
-        calendar_today
-      </span>
+  const location = useLocation();
+  
+  const getIconColor = (path) => {
+    return location.pathname === path ? 'purple' : 'lightGray';
+  };
 
-      <span className="material-symbols-outlined">
-        shopping_cart
-      </span>
 
-      <span className="material-symbols-outlined">
-        home
-      </span>
+    return (
 
-      <span className="material-symbols-outlined">
-        settings
-      </span>
-
-      <span className="material-symbols-outlined">
-        family_restroom
-      </span>
-    </div>
-  );
+  <nav className={Style.divFooterMain}>
+  <Link to="/Home"><Home className="material-symbols-outlined" style={{ color: getIconColor('/Home') }} /></Link>
+  <Link to="/Teste"><ShoppingCart className="material-symbols-outlined" style={{ color: getIconColor('/Teste') }} /></Link>
+  <Settings className="material-symbols-outlined" style={{ color: getIconColor('/settings') }} />
+  <CalendarToday className="material-symbols-outlined" style={{ color: getIconColor('/calendar') }} />
+  <FamilyRestroom className="material-symbols-outlined" style={{ color: getIconColor('/family') }} />
+</nav>
+);
 }
 
 export default FooterMain;
+
+
+
+
+
+ //     <nav className={Style.divFooterMain}>
+  //       <calendar_today className="material-symbols-outlined" >
+  //         calendar_today
+  //       </calendar_today>
+
+  //       <shopping_cart className="material-symbols-outlined" >
+  //         shopping_cart
+  //       </shopping_cart>
+
+  //       <home className="material-symbols-outlined">
+  //         home
+  //       </home>
+
+  //       <settings className="material-symbols-outlined">
+  //         settings
+  //       </settings>
+
+  //       <family_restroom className="material-symbols-outlined">
+  //         family_restroom
+  //       </family_restroom>
+  //     </nav>
+  // );
---
+++ src/components/teste.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeaderMain from './HeaderMain'
+import FoorterLogCad from './FoorterLogCad'
+import FooterMain from './FooterMain'
+
+const teste = () => {
+  return (
+    <div>
+        <HeaderMain/>
+
+    <div>teste pipo</div>
+    
+        <FooterMain/>
+    </div>
+  )
+}
+
+export default teste
