7d5f3056449f5d63e99e4d0270fff647cc4c0bf3
Arthur Evangelista Oliveira <arthur.e.oliveira@aluno.senai.br>
2024-10-04 17:28:14 -0300

    Fazendo pagina home corretamente

--- src/components/CardChildren.jsx
+++ src/components/CardChildren.jsx
@@ -3,6 +3,8 @@ import style from '../styles/CardChildren.module.css';
 
 const CardChildren = () => {
 
+  
+
   const[children, setChildren] =useState([
     {
       id:1,
@@ -21,7 +23,7 @@ const CardChildren = () => {
         {taskName: "Fazer dever de matematica" , points: 40, complete:false},
         {taskName: "Dormir mais cedo" , points: 10, complete:false}
       ],
-      totalPoints: 100
+      totalPoints: 50
     },
 
     {
@@ -31,18 +33,36 @@ const CardChildren = () => {
         {taskName: "Arrumar a casa" , points: 80, complete:false},
         {taskName: "Ir passear com o cachorro" , points: 30, complete:false}
       ],
-      totalPoints: 100
+      totalPoints: 70
     }
   ])
 
+
   return (
+    // Criei esta div apenas para colocar uma cor de fundo roxa bem claro e deixar as divs roxa com a div branca dentro
     <div className={style.homeMain}>
+      {/* div criada para estilizar a parte roxa que dentro dela tem a div branca  */}
       <div className={style.homePurple}>
-        <div className={style.homeChildren}>
-          <div className={style.nameChildren}>
-            
+        {children.map((filho, index) => (
+          // Div criada de maneira dinamica e estilizando todas as informações dos filhos
+          <div key={filho.id} style={{backgroundColor:'#FFFFFF' , width:'90%', height:'30%', display:'flex', justifyContent:"center" , flexDirection:'column', gap:'1rem', alignItems:"center", textAlign:"center"}}>
+            <div className={style.nameChildren}>
+              <h1>{filho.name}</h1>
+              <p>Total de pontos: {filho.totalPoints}</p>
+            </div>
+            <div className={style.taskChildren}>
+              <form>
+                  <input
+                    type="checkbox"
+                    id="subscribeNews"
+                    name="subscribe"
+                    value="newsletter" />
+                  <label for="subscribeNews"> Subscreva a newsletter? </label>
+              </form>
+            </div>
+               
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
