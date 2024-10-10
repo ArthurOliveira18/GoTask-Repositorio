193f97db05f1d05924ed5ba2a24e9605a0ac360a
Arthur Evangelista Oliveira <arthur.e.oliveira@aluno.senai.br>
2024-10-10 17:02:36 -0300

    Iniciando tela de loja de pontos

--- src/components/ItensStore.jsx
+++ src/components/ItensStore.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import style from '../styles/ItensStore.module.css'
 
 const ItensStore = () => {
+
+  const [recompensas, setRecompensas] = useState([
+    {
+      id: 1,
+      name: 'Dormir duas horas mais tarde',
+      points: 30
+
+    },
+
+    {
+      id: 2,
+      name: 'Dormir duas horas mais tarde',
+      points: 30
+
+    },
+    {
+      id: 3,
+      name: 'Dormir duas horas mais tarde',
+      points: 30
+
+    },
+    {
+      id: 4,
+      name: 'Dormir duas horas mais tarde',
+      points: 30
+
+    },
+    {
+      id: 5,
+      name: 'Dormir duas horas mais tarde',
+      points: 30
+
+    },
+    {
+      id: 6,
+      name: 'Dormir duas horas mais tarde',
+      points: 30
+
+    },
+    
+  ])
+
   return (
-    <div>ItensStore</div>
+    <div className={style.itensBlue}>
+      {recompensas.map((recomp) =>(
+        <div key={recomp.id} style={{color:"#000" , width:"30%",height:'20%',backgroundColor: "#fff" }}>
+          <p>a</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
