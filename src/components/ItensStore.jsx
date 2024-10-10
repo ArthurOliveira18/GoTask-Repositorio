0c0e2cdd96c717b22740719075165c593f242f2f
Arthur Evangelista Oliveira <arthur.e.oliveira@aluno.senai.br>
2024-10-10 17:28:48 -0300

    Quase finalizando

--- src/components/ItensStore.jsx
+++ src/components/ItensStore.jsx
@@ -13,32 +13,32 @@ const ItensStore = () => {
 
     {
       id: 2,
-      name: 'Dormir duas horas mais tarde',
-      points: 30
+      name: 'Sair com os amigos',
+      points: 70
 
     },
     {
       id: 3,
-      name: 'Dormir duas horas mais tarde',
-      points: 30
+      name: 'Brincar mais tempo com os amigos',
+      points: 10
 
     },
     {
       id: 4,
-      name: 'Dormir duas horas mais tarde',
-      points: 30
+      name: 'Jogar por mais de uma hora',
+      points: 20
 
     },
     {
       id: 5,
-      name: 'Dormir duas horas mais tarde',
-      points: 30
+      name: 'Ir ao cinema',
+      points: 90
 
     },
     {
       id: 6,
-      name: 'Dormir duas horas mais tarde',
-      points: 30
+      name: 'Vê desenho até mais tarde',
+      points: 25
 
     },
     
@@ -47,9 +47,27 @@ const ItensStore = () => {
   return (
     <div className={style.itensBlue}>
       {recompensas.map((recomp) =>(
-        <div key={recomp.id} style={{color:"#000" , width:"30%",height:'20%',backgroundColor: "#fff" }}>
-          <p>a</p>
+        <div key={recomp.id} style={{color:"#000" , 
+        width:"40%",
+        height:'20%',
+        backgroundColor: "#fff",
+        display:'flex',
+        justifyContent:'center',
+        textAlign:'center',
+        flexDirection:'column',
+        alignItems:'center',
+        border: '5px solid #000',
+        borderBottom:'10px solid #000',
+        borderRadius:'1rem'
+
+        
+        }}>
+          <p>{recomp.name}</p>
+          <br />
+          <p>Pontos:</p>
+          <h2>{recomp.points} P</h2>
         </div>
+        
       ))}
     </div>
   )
