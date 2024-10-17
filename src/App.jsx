600b19a3338c36bc792f19b8636dfeadcae8aae8
Arthur Evangelista Oliveira <arthur.e.oliveira@aluno.senai.br>
2024-10-17 17:27:37 -0300

    iniciado o db json

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0);
-
+  
   return (
     <Router>  {/* Aqui inicia o Router */}
       <div>
