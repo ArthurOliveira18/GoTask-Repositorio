bb480dad3276aa027c82c9a2d6379880199e0d23
Arthur Evangelista Oliveira <arthur.e.oliveira@aluno.senai.br>
2024-11-08 15:12:42 -0300

    testando pra ver se vai

---
+++ server/controllers/historyTaskController.js
@@ -0,0 +1,24 @@
+const pool = require('../db/db');
+
+
+const getUsers = async (req, res) => {
+  try {
+    const [rows] = await pool.query('SELECT * FROM users');
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao buscar usuários', error });
+  }
+};
+
+
+const createUser = async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const [result] = await pool.query('INSERT INTO users (name, email) VALUES (?, ?)', [name, email]);
+    res.json({ id: result.insertId, name, email });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao criar usuário', error });
+  }
+};
+
+module.exports = { getUsers, createUser };
--- server/index.js
+++ server/index.js
@@ -1,23 +1,35 @@
 // Codigo do banco de dados... Sinceramente? não sei nem por onde começar. Arthur do futuro, deu tudo certo?
 const express = require("express");
 const app = express();
-const port = 3000;
+// const conn = require('./db/db')
+const usersRoutes = require('./routes/usuariosRoutes')
+const historyTaskRoutes = require('./routes/historyTaskRoutes')
 
 const path = require("path");
 const basePath = path.join(__dirname, "pages");
 
-const mysql = require("mysql2");
+const port = 5000
+
+// const mysql = require("mysql2");
 const exp = require("constants");
 
+app.use("/users", usersRoutes)
+app.use("/", historyTaskRoutes)
+
+const mysql = require('mysql2');
 
 
 const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "gotask",
-  port: 3306
-});
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "gotask",
+    port: 3306
+  });
+
+app.get("/", (req, res) => {
+  res.send("Conectado")
+})
 
 conn.connect((erro) => {
   if (erro) {
